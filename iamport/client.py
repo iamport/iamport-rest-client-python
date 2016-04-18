18cc33f42b910601f4b40d17df4f4db6405034bb
pcompassion <p.compassion@gmail.com>
2016-04-18 22:48:55 +0900

    부분 취소 (cancel amount)

--- iamport/client.py
+++ iamport/client.py
@@ -88,23 +88,27 @@ class Iamport(object):
 
         return self._post(url, kwargs)
 
-    def cancel_by_merchant_uid(self, merchant_uid, reason):
+    def cancel_by_merchant_uid(self, merchant_uid, reason, **kwargs):
         payload = {'merchant_uid': merchant_uid, 'reason': reason}
+        if kwargs:
+            payload = payload.update(kwargs)
         return self._cancel(payload)
 
-    def cancel_by_imp_uid(self, imp_uid, reason):
+    def cancel_by_imp_uid(self, imp_uid, reason, **kwargs):
         payload = {'imp_uid': imp_uid, 'reason': reason}
+        if kwargs:
+            payload = payload.update(kwargs)
         return self._cancel(payload)
 
     def cancel(self, reason, **kwargs):
-        merchant_uid = kwargs.get('merchant_uid')
-        if merchant_uid:
-            return self.cancel_by_merchant_uid(merchant_uid, reason)
-        try:
-            imp_uid = kwargs['imp_uid']
-        except KeyError:
+        imp_uid = kwargs.pop('imp_uid', None)
+        if imp_uid:
+            return self.cancel_by_imp_uid(imp_uid, reason, **kwargs)
+
+        merchant_uid = kwargs.pop('merchant_uid', None)
+        if merchant_uid is None:
             raise KeyError('merchant_uid or imp_uid is required')
-        return self.cancel_by_imp_uid(imp_uid, reason)
+        return self.cancel_by_merchant_uid(merchant_uid, reason, **kwargs)
 
     def is_paid(self, amount, **kwargs):
         response = kwargs.get('response')
